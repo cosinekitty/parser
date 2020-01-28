d648ae54389a3a4f059f2331f20c13ee9b11562b
Don Cross <cosinekitty@gmail.com>
2020-01-27 20:08:04 -0500

    Starting to add ability to convert parsed expression to Latex.

--- expression.js
+++ expression.js
@@ -31,63 +31,158 @@ window.onload = function() {
     }
 
     class Expression {
-        constructor(optoken, arglist) {
+        constructor(precedence, optoken, arglist) {
+            this.precedence = precedence;
             this.optoken = optoken;
             this.arglist = arglist;
+        }
+
+        Latex() {
+            throw {name:'InternalError', message:'Do not know how to convert expression to Latex.'};
+        }
+
+        Latex_Binary_LeftAssoc() {
+            let left = this.arglist[0].Latex();
+            let right = this.arglist[1].Latex();
+
+            // Use parentheses around the left child expression
+            // if its operator precedence is less than this node's precedence.
+            // If it is equal, assume left-associativity means parentheses are not needed.
+            if (this.arglist[0].precedence < this.precedence) {
+                left = '\\left(' + left + '\\right)';
+            }
+
+            // Use parentheses around the right child expression
+            // if operator precedence is lower or equal.
+            // Even if equal, parentheses are needed because we are
+            // overriding left-associativity.
+            if (this.arglist[1].precedence <= this.precedence) {
+                right = '\\left(' + right + '\\right)';
+            }
+
+            return left + this.optoken.text + right;
+        }
+
+        Latex_Binary_RightAssoc() {
+            // Similar to left associative, only we use
+            // parentheses when the left child expression
+            // has equal precedence, not the right.
+
+            let left = this.arglist[0].Latex();
+            let right = this.arglist[1].Latex();
+
+            if (this.arglist[0].precedence <= this.precedence) {
+                left = '\\left(' + left + '\\right)';
+            }
+
+            if (this.arglist[1].precedence < this.precedence) {
+                right = '\\left(' + right + '\\right)';
+            } else {
+                right = '{' + right + '}';  // for exponentiation
+            }
+
+            return left + this.optoken.text + right;
         }
     }
 
     class Expression_Add extends Expression {
         constructor(optoken, left, right) {
-            super(optoken, [left, right]);
+            super(1, optoken, [left, right]);
+        }
+
+        Latex() {
+            return this.Latex_Binary_LeftAssoc();
         }
     }
 
     class Expression_Subtract extends Expression {
         constructor(optoken, left, right) {
-            super(optoken, [left, right]);
+            super(1, optoken, [left, right]);
+        }
+
+        Latex() {
+            return this.Latex_Binary_LeftAssoc();
         }
     }
 
     class Expression_Multiply extends Expression {
         constructor(optoken, left, right) {
-            super(optoken, [left, right]);
+            super(2, optoken, [left, right]);
+        }
+
+        Latex() {
+            return this.Latex_Binary_LeftAssoc();
         }
     }
 
     class Expression_Divide extends Expression {
         constructor(optoken, left, right) {
-            super(optoken, [left, right]);
+            super(2, optoken, [left, right]);
+        }
+
+        Latex() {
+            // Use fraction notation. Operator precedence is irrelevant.
+            return '\\frac{' + this.arglist[0].Latex() + '}{' + this.arglist[1].Latex() + '}';
         }
     }
 
     class Expression_Power extends Expression {
         constructor(optoken, left, right) {
-            super(optoken, [left, right]);
+            super(4, optoken, [left, right]);
+        }
+
+        Latex() {
+            return this.Latex_Binary_RightAssoc();
         }
     }
 
     class Expression_Negative extends Expression {
         constructor(optoken, arg) {
-            super(optoken, [arg]);
+            super(3, optoken, [arg]);
+        }
+
+        Latex() {
+            // Unary prefix operator.
+            let argtext = this.arglist[0].Latex();
+            if (this.arglist[0].precedence < this.precedence) {
+                return '-\\left(' + argtext + '\\right)';
+            }
+            return '-' + argtext;
         }
     }
 
     class Expression_Identifier extends Expression {
         constructor(token) {
-            super(token, []);
+            super(9, token, []);
+        }
+
+        Latex() {
+            return this.optoken.text;
         }
     }
 
     class Expression_Number extends Expression {
         constructor(token) {
-            super(token, []);
+            super(9, token, []);
+        }
+
+        Latex() {
+            const m = this.optoken.text.match(/^([^eE]+)[eE](.*)$/);
+            if (m) {
+                // Convert scientific notation:  1.23e-4 ==> 1.23 \times 10^{-4}
+                return m[1] + ' \\times 10^{' + m[2] + '}';
+            }
+            return this.optoken.text;
         }
     }
 
     class Expression_Function extends Expression {
         constructor(token, arglist) {
-            super(token, arglist);
+            super(9, token, arglist);
+        }
+
+        Latex() {
+            return '\\' + this.optoken.text + '\\left(' + this.arglist.map(child => child.Latex()).join(', ') + '\\right)';
         }
     }
 
@@ -133,7 +228,7 @@ window.onload = function() {
             let expr = this.ParsePowExpr();
             let optoken;
             while (optoken = this.NextTokenIs(['*', '/'])) {
-                const right = ParsePowExpr();
+                const right = this.ParsePowExpr();
                 if (optoken.text === '*') {
                     expr = new Expression_Multiply(optoken, expr, right);
                 } else {
@@ -238,6 +333,6 @@ window.onload = function() {
     processButton.addEventListener('click', function(){
         const parser = new Parser(textInput.value);
         const expr = parser.Parse();
-        console.log(expr);
+        console.log(expr.Latex());
     });
 }
